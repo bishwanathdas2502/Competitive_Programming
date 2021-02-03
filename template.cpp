446546403187fd394d81783d138427c5b22d5233
Bishwanath Das <42764104+bishwanathdas2502@users.noreply.github.com>
2021-02-03 22:56:50 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -2,16 +2,37 @@
 #include <ext/pb_ds/assoc_container.hpp>
 using namespace __gnu_pbds;
 using namespace std;
-template<typename T>
-void debug(string t,T input){
-    cout<<t<<"->"<<input<<"\n";
-}
-//for loop
-template<typename T>
-void debug(int i,string t,T input){
-    cout<<"Case#"<<i<<" "<<t<<"->"<<input<<"\n";
-}
 
+/*============ D E B U G ==============*/
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+
+/*================ M A C R O S ==================  */
 #define ff              first
 #define ss              second
 #define int             long long
@@ -30,7 +51,8 @@ void debug(int i,string t,T input){
 #define mk(arr,n,type)  type *arr=new type[n];
 #define w(x)            int x; cin>>x; while(x--)
 #define rep(i,a,b)      for(int i=a;i<b;i++)
-#define rrep(i,a,b)     for(int i=a;i>=b;i--)   
+#define rrep(i,a,b)     for(int i=a;i>=b;i--) 
+#define all(z)          z.begin(),z.end()  
 #define FIO             ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
 mt19937                 rng(chrono::steady_clock::now().time_since_epoch().count());
 
@@ -38,12 +60,14 @@ mt19937                 rng(chrono::steady_clock::now().time_since_epoch().count
 // two function are order_of_key(num)--> returns index of number after sorting
 //*(itr).find_by_order(3) --> return the num after which the number is inserted
 typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+
+/*============== G R A P H =============== */
 // vector<int> adj[(int)1e6];
 // queue<int> q;
 // int col[(int)1e6];
 // int vis[(int)1e6];
 
-
+/*==============S O L U T I O N============*/
 void solve(){
 
 }
