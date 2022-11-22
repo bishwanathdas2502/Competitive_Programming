a365ebf6e560c83d5f45e053bcd8ec0245f14a82
Bishwanath Das <42764104+bishwanathdas2502@users.noreply.github.com>
2022-11-22 17:08:47 +0530

    template updated
    new template

--- template.cpp
+++ template.cpp
@@ -1,86 +1,92 @@
-#include<bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-using namespace __gnu_pbds;
+#include <bits/stdc++.h>
 using namespace std;
 
-/*============ D E B U G ==============*/
-void __print(int x) {cerr << x;}
-void __print(long x) {cerr << x;}
-void __print(long long x) {cerr << x;}
-void __print(unsigned x) {cerr << x;}
-void __print(unsigned long x) {cerr << x;}
-void __print(unsigned long long x) {cerr << x;}
-void __print(float x) {cerr << x;}
-void __print(double x) {cerr << x;}
-void __print(long double x) {cerr << x;}
-void __print(char x) {cerr << '\'' << x << '\'';}
-void __print(const char *x) {cerr << '\"' << x << '\"';}
-void __print(const string &x) {cerr << '\"' << x << '\"';}
-void __print(bool x) {cerr << (x ? "true" : "false");}
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define int long long
+#define FIO ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define rep(ii, a, b) for (int ii = a; ii < b; ii++)
+#define rrep(ii, a, b) for (int ii = a; ii >= b; ii--)
+#define to_upper(a) transform(a.begin(),a.end(),a.begin(),::toupper);
+#define to_lower(b) transform(b.begin(),b.end(),b.begin(),::tolower);
+#define endl '\n'
 
-template<typename T, typename V>
-void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
-template<typename T>
-void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
-void _print() {cerr << "]\n";}
-template <typename T, typename... V>
-void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
-#ifndef ONLINE_JUDGE
-#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
-#else
-#define debug(x...)
-#endif
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
 
 
-/*================ M A C R O S ==================  */
-#define ff              first
-#define ss              second
-#define int             long long
-#define pb              push_back
-#define mp              make_pair
-#define pii             pair<int,int>
-#define vi              vector<int>
-#define mii             map<int,int>
-#define pqb             priority_queue<int>
-#define pqs             priority_queue<int,vi,greater<int> >
-#define setbits(x)      __builtin_popcountll(x)
-#define zrobits(x)      __builtin_ctzll(x)
-#define mod             1000000007
-#define inf             1e18
-#define ps(x,y)         fixed<<setprecision(y)<<x
-#define mk(arr,n,type)  type *arr=new type[n];
-#define w(x)            int x; cin>>x; while(x--)
-#define rep(i,a,b)      for(int i=a;i<b;i++)
-#define rrep(i,a,b)     for(int i=a;i>=b;i--) 
-#define all(z)          z.begin(),z.end()  
-#define FIO             ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
-mt19937                 rng(chrono::steady_clock::now().time_since_epoch().count());
 
-//pbds similar to sets
-// two function are order_of_key(num)--> returns index of number after sorting
-//*(itr).find_by_order(3) --> return the num after which the number is inserted
-typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+#define debug(x) cerr << #x<<" "; _print(x); cerr << endl;
 
-/*============== G R A P H =============== */
-// vector<int> adj[(int)1e6];
-// queue<int> q;
-// int col[(int)1e6];
-// int vis[(int)1e6];
 
-/*==============S O L U T I O N============*/
-void solve(){
+void _print(ll t) {cerr << t;}
+
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+/*---------------------------------------------------------------------------------------------------------------------------*/
+ll gcd(ll a, ll b) {if (b > a) {return gcd(b, a);} if (b == 0) {return a;} return gcd(b, a % b);}
+ll expo(ll a, ll b, ll mod) {ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
+void extendgcd(ll a, ll b, ll*v) {if (b == 0) {v[0] = 1; v[1] = 0; v[2] = a; return ;} extendgcd(b, a % b, v); ll x = v[1]; v[1] = v[0] - v[1] * (a / b); v[0] = x; return;} //pass an arry of size1 3
+ll mminv(ll a, ll b) {ll arr[3]; extendgcd(a, b, arr); return arr[0];} //for non prime b
+ll mminvprime(ll a, ll b) {return expo(a, b - 2, b);}
+bool revsort(ll a, ll b) {return a > b;}
+ll combination(ll n, ll r, ll m, ll *fact, ll *ifact) {ll val1 = fact[n]; ll val2 = ifact[n - r]; ll val3 = ifact[r]; return (((val1 * val2) % m) * val3) % m;}
+
+vector<ll> sieve(int n) {int*arr = new int[n + 1](); vector<ll> vect; for (int i = 2; i <= n; i++)if (arr[i] == 0) {vect.push_back(i); for (int j = 2 * i; j <= n; j += i)arr[j] = 1;} return vect;}
+ll mod_add(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
+ll mod_mul(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
+ll mod_sub(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
+ll mod_div(ll a, ll b, ll m) {a = a % m; b = b % m; return (mod_mul(a, mminvprime(b, m), m) + m) % m;}  //only for prime m
+
+
+
+
+
+
+
+void solve()
+{
+    int n;
+    cin >> n;
+    
+    
 
 }
 
-
-
-int32_t main(){
+int32_t main()
+{
     FIO;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+
+    freopen("output.txt", "w", stdout);
+
+    freopen("error.txt","w",stderr);
+
+#endif
+
     int t = 1;
-    cin>>t;
-    while(t--){
+    cin >> t;
+    while (t--)
+    {
         solve();
     }
-    
     return 0;
 }
