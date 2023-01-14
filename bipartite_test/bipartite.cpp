fdaca956f21e413b930bad797aa255dd55588e84
Bishwanath Das <42764104+bishwanathdas2502@users.noreply.github.com>
2023-01-14 13:12:21 +0530

    bipartite test
    bipartite test along with count of number of node with white color and black color
    if ret.first == -1 then the graph is not bipartite

---
+++ bipartite_test/bipartite.cpp
@@ -0,0 +1,40 @@
+vector<int> G[200005];
+int color[200005];
+
+
+
+
+//bipartite test
+pair<int,int> dfs(int node,int par,int c){
+	
+	pair<int,int> ret = {0,0};
+	color[node] = c;
+
+	if(c == 1){
+		ret.first++;
+	}
+	else{
+		ret.second++;
+	}
+
+
+	for(auto child : G[node]){
+
+		//if child == parent color of child is opposite to node
+		if(child == par||color[child] == -c)continue;
+
+		//if color of child is same as parent return false
+		if(color[child] == c) return {-1LL,-1LL};
+
+		pair<int,int> res = dfs(child,node,-c);
+		
+		//if child is not satisfying 2 coloring
+		if(res.first == -1LL)return {-1LL,-1LL};
+
+		//if satisfy and color 1/black to ret.first and 0/white to ret.second
+		ret.first += res.first,ret.second += res.second;
+
+	}
+	return ret;
+
+}
