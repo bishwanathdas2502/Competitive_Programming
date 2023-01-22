b726d54f85f9bfecfa8f5787087b710f3a87fcec
Bishwanath Das <42764104+bishwanathdas2502@users.noreply.github.com>
2023-01-22 21:00:34 +0530

    Update floydwarshall.cpp

--- FLOYD WARSHALL ALGO/floydwarshall.cpp
+++ FLOYD WARSHALL ALGO/floydwarshall.cpp
@@ -18,3 +18,38 @@ for(int k = 1;k<=n;k++){
         }
     }
 }
+
+
+
+// When we want to minimise the distance along with that we want to minimise/maximise other value
+const int N = 505;
+
+int dist[N][N];
+int val[N][N];
+
+int arr[N];
+void floyd(int n){
+
+    for(int k = 0;k<n;k++){
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<n;j++){
+                int prev = dist[i][j];
+                int new_d = dist[i][k] + dist[k][j];
+
+
+                if(prev < new_d){
+                    continue;
+                }
+                //here we want to minimse or maximise the other value
+                else if(prev == new_d){
+                    val[i][j] = max(val[i][j],val[i][k] + val[k][j]);
+                }
+                else{
+                    dist[i][j] = new_d;
+                    val[i][j] = val[i][k] + val[k][j];
+                }
+            }
+        }
+    }
+
+}
