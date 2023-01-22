e662f886ee9a5ec1b4f6eebec129c5aa06ba911a
Bishwanath Das <42764104+bishwanathdas2502@users.noreply.github.com>
2023-01-22 16:51:59 +0530

    Update floydwarshall.cpp

--- FLOYD WARSHALL ALGO/floydwarshall.cpp
+++ FLOYD WARSHALL ALGO/floydwarshall.cpp
@@ -1,1 +1,20 @@
 
+
+//graph input
+rep(i,0,m){
+        int a,b,wt;cin>>a>>b>>wt;
+        dist[a][b] = wt;
+
+    }
+
+
+
+//uncomment the if condition in case of negative weight in graph
+for(int k = 1;k<=n;k++){
+    for(int i = 1;i<=n;i++){
+        for(int j = 1;j<=n;j++){
+            // if(dist[i][k] != INF && dist[k][j] != INF)
+            dist[i][j] = min(dist[i][j],dist[i][k] + dist[k][j]);
+        }
+    }
+}
