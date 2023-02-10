804f0800e8e157be6ce15741fc32c5bf16f407c5
Bishwanath Das <42764104+bishwanathdas2502@users.noreply.github.com>
2023-02-10 09:04:54 +0530

    Create subset.cpp

---
+++ SUBSET GENERATION/subset.cpp
@@ -0,0 +1,19 @@
+//first initilise the array on which subset is required
+vector<int> arr;
+//intialise the above array
+
+
+int k = arr.size();
+vector<ll> res;
+
+for(ll i = 0;i<(1LL << k);i++){
+    
+    for(int j = 0;j<k;j++){
+        if(i&(1LL<<j)){
+            //define the operation on subset
+        }
+    }
+    res.push_back(cur);
+}
+    
+
