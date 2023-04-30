ee1047f064b67aa731b4d602c0cfa25fb12b70d7
Bishwanath Das <42764104+bishwanathdas2502@users.noreply.github.com>
2023-04-30 12:46:34 +0530

    object oriented dsu to create multiple objects

---
+++ DISJOINT SET UNION/dsu_object.cpp
@@ -0,0 +1,46 @@
+
+
+
+
+class DSU{
+	
+vector<int> par;
+vector<int> rank;
+
+public:
+    DSU(int n){
+        par.push_back(0);
+        rank.push_back(0);
+        for(int i=1;i<=n;i++){
+            par.push_back(i);
+            rank.push_back(0);
+        }
+    }
+
+    int findPar(int node){
+        if(node == par[node]){
+            return node;
+        }
+
+        return par[node] = findPar(par[node]);
+    }
+
+    void Union(int node1,int node2){
+        int u  = findPar(node1);
+        int v = findPar(node2);
+
+        if(rank[u] < rank[v]){
+            par[u] = v;
+        }
+        else if(rank[u] > rank[v]){
+            par[v] = u;
+        }
+        else{
+            par[v] = u;
+            rank[u]++;
+        }
+    }
+
+
+
+};
