205758e9f8a2694fd1344696c31d42234e02909b
Bishwanath Das <42764104+bishwanathdas2502@users.noreply.github.com>
2023-10-05 22:03:54 +0530

    Create deletenode.cpp

---
+++ TREE/deletenode.cpp
@@ -0,0 +1,18 @@
+//technique to handle delete a node
+
+
+TreeNode* dfs(TreeNode* root){
+    if(root == NULL)
+        return NULL;
+
+    r->left = dfs(root->left);
+    r->right = dfs(root->right);
+
+    //condition
+    //true
+    //return NULL
+
+    //else
+    //return root
+    
+}
